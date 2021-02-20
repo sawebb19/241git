ae99815256a80e35a03e6cc16c89b601bc4f2596
SAWebb19 <webbsamu@mail.gvsu.edu>
2021-02-20 20:16:14 +0000

    Update makeProject.sh

--- makeProject.sh
+++ makeProject.sh
@@ -1,13 +1,7 @@
 mkdir $1
 cd $1
 git init
-mkdir bin
-mkdir include
-mkdir lib
-mkdir share
-mkdir man
-mkdir info
-touch README.md
+mkdir bin include lib share man info
 echo $1 >> README.md
 touch .gitignore
 git add -A
