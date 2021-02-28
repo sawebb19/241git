b1a98427efbb556a176ff52c2605ed115c4ce5ad
SAWebb19 <webbsamu@mail.gvsu.edu>
2021-02-28 19:52:49 +0000

    Fix hwinfo date function

--- sed_project/hwinfo.sh
+++ sed_project/hwinfo.sh
@@ -5,11 +5,14 @@ Memory:
 ======="
 echo "     total              used                free                 shared              buff/cache          available"
 free -h | sed -nE 's/Gi/ Gigabytes/g;s/Mi/ Megabytes/g;s/[ ]+/ /;2p'
-echo ""
-echo "Disks:
+echo "
+Disks:
 ======"
 df -h | sed -nE '/.*[ ]+[0-9]*\.?[0-9]?T/p'
 echo "
 Date:
 ====="
-date | sed -nE 's/Sun /Sunday,#/;s/Mon /Monday,#/;s/Tue /Tuesday,#/;s/Wed /Wednesday,#/;s/Thu /Thursday,#/;s/Fri /Friday,#/;s/Sat /Saturday,#/;s/ Jan /#January#/;s/ Feb /#February#/;s/ Mar /#March#/;s/ Apr /#April#/;s/ Jun /#June#/;s/ Jul /#July#/;s/ Aug /#August#/;s/ Sep /#September#/;s/ Oct /#October#/;s/ Nov /#November#/;s/ Dec /#December#/;s/ /./;s/#/ /g;s/AM/a.m./;s/EST/Eastern Standard/;p'
+
+# date | sed -nE 's/Sun /Sunday,#/;s/Mon /Monday,#/;s/Tue /Tuesday,#/;s/Wed /Wednesday,#/;s/Thu /Thursday,#/;s/Fri /Friday,#/;s/Sat /Saturday,#/;s/ Jan /#January#/;s/ Feb /#February#/;s/ Mar /#March#/;s/ Apr /#April#/;s/ Jun /#June#/;s/ Jul /#July#/;s/ Aug /#August#/;s/ Sep /#September#/;s/ Oct /#October#/;s/ Nov /#November#/;s/ Dec /#December#/;s/ /./;s/#/ /g;s/AM/a.m./;s/PM/p.m./;s/EST/Eastern Standard/;p'
+
+date +%A," "%d" "%B" "%Y.%T" "%P" "%Z | sed -nE 's/am/a.m./;s/pm/p.m./;s/EST/Eastern Standard/;s/UTC/Coordinated Universal/;p'
